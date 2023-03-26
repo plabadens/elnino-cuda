fix(scaling.py): fix segfaults in CUDA code

scaling.py
@@ -14,15 +14,21 @@ def run_weak_scaling(
     ny = np.int32(n_new / nx)
 
     all_times = []
-    base_args = ["./src/elnino_cuda", "--iter", str(iterations)]
+    base_args = [
+        "./src/elnino_cuda",
+        "--iter",
+        str(iterations),
+        "--fperiod",
+        str(iterations),
+    ]
 
     if not threads is None:
-        base_args += ["--threads", str(threads), "--fperiod", str(iterations)]
+        base_args += ["--threads", str(threads)]
 
     print("Starting single-threaded runs")
     for x, y, b in zip(nx, ny, blocks):
         args = base_args + ["--nx", str(x), "--ny", str(y), "--blocks", "1"]
-        print(args)
+        print(" ".join(args))
 
         process = subprocess.Popen(args, stdout=subprocess.PIPE)
 
@@ -30,14 +36,13 @@ def run_weak_scaling(
 
         match = re.search(output_pattern, output)
 
-        if not match is None:
-            output_value = float(match.group(1))
-            all_times.append(output_value)
+        output_value = float(match.group(1))
+        all_times.append(output_value)
 
-    print("Starting scaling runs")
+    print("---\nStarting scaling runs")
     for x, y, b in zip(nx, ny, blocks):
         args = base_args + ["--nx", str(x), "--ny", str(y), "--blocks", str(b)]
-        print(args)
+        print(" ".join(args))
 
         for _ in range(samples):
             process = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -46,13 +51,14 @@ def run_weak_scaling(
 
             match = re.search(output_pattern, output)
 
-            if not match is None:
-                output_value = float(match.group(1))
-                all_times.append(output_value)
+            output_value = float(match.group(1))
+            all_times.append(output_value)
 
     df = pd.DataFrame(
         {
-            "blocks": np.concatenate((np.repeat(1, len(blocks)), np.repeat(blocks, samples))),
+            "blocks": np.concatenate(
+                (np.repeat(1, len(blocks)), np.repeat(blocks, samples))
+            ),
             "iter": np.repeat(iterations, len(blocks) * samples + len(blocks)),
             "nx": np.concatenate((nx, np.repeat(nx, samples))),
             "ny": np.concatenate((nx, np.repeat(ny, samples))),
@@ -64,5 +70,5 @@ def run_weak_scaling(
 
 
 if __name__ == "__main__":
-    data = run_weak_scaling(min_blocks=1, max_blocks=32, iterations=10000, threads=1)
+    data = run_weak_scaling(min_blocks=1, max_blocks=32, iterations=10)
     data.to_csv("weak_scaling.csv", index=False)